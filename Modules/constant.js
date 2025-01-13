a0673ec06f05b686db6e670dd433b15d4c67f541
Angie Barreto <angiepbg13@gmail.com>
2025-01-13 16:13:27 -0500

    Module exercises and their respective imports/exports are added.

---
+++ Modules/constant.js
@@ -0,0 +1,17 @@
+export const PI = Math.PI;
+
+export let user = "Ana";
+const password = "Planta";
+//  export default password;
+
+const hello = () => console.log ("Hola");
+
+export function greet(){
+console.log("Hola modulos")
+};
+
+export default class Saludar {
+    constructor(){
+        console.log("hola clases.")
+    }
+}
